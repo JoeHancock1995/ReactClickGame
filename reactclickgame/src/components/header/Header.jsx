e67556b9abbc734332a791adabca97f4def5a8a6
Joe Hancock <joe.hancock95@gmail.com>
2019-09-04 20:10:50 -0500

    put in es6

--- reactclickgame/src/components/header/Header.jsx
+++ reactclickgame/src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Header(){
-    return <h1>Card Click Game</h1>
+// function Header(){
+//     return <h1>Card Click Game</h1>
+// }
+const Header = () => {
+return (
+    <div>
+        <h1>Card Click Game</h1>
+    </div>
+    )
 }
+
 
 export default Header;
