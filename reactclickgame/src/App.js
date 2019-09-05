36fa461ac47b67ca919d2ec483963dac5af8adfa
Joe Hancock <joe.hancock95@gmail.com>
2019-09-05 02:45:29 -0500

    added some div

--- reactclickgame/src/App.js
+++ reactclickgame/src/App.js
@@ -6,11 +6,51 @@ import Score from './components/header/Score';
 
 import './styles/main.css';
 
+// const clickbox = [
+//   {
+//     id: 1,
+//   },
+//   {
+//     id: 2,
+//   },
+//   {
+//     id: 3,
+//   },
+//   {
+//     id: 4,
+//   },
+//   {
+//     id: 5,
+//   },
+//   {
+//     id: 6,
+//   },
+//   {
+//     id: 7,
+//   },
+//   {
+//     id: 8,
+//   },
+//   {
+//     id: 9,
+//   },
+//   {
+//     id: 10,
+//   },
+//   {
+//     id: 11,
+//   },
+//   {
+//     id: 12,
+//   }
+// ];
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <Score/>
+
     </div>
   );
 }
--- reactclickgame/src/components/card/Card.jsx
+++ reactclickgame/src/components/card/Card.jsx
@@ -1,4 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-
+class Card extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+render() {
+// const notClicked = props.clickboxes.filter(clickbox => !clickbox.clicked);
+    return (
+          <button className="square" onClick={function() { alert('click');}}>
+           {this.props.value}     
+          </button>
+    )
+    }
+};
 export default Card;
