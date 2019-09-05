3f855bdb1f1aa42ba4b7ff6cc4f450d190ad9750
Joe Hancock <joe.hancock95@gmail.com>
2019-09-04 19:51:33 -0500

    Header test

--- reactclickgame/src/App.js
+++ reactclickgame/src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import Card from './components/card/Card';
 import Header from './components/header/Header';
-import Score from './components/header/Score';
-import Footer from './components/footer/Footer';
+// import Score from './components/header/Score';
+// import Card from './components/card/Card';
+// import Footer from './components/footer/Footer';
 
 import './styles/main.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
--- reactclickgame/src/components/header/Header.jsx
+++ reactclickgame/src/components/header/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
 
+function Header(){
+    return <h1>Card Click Game</h1>
+}
 
 export default Header;
