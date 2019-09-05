7b9c39d71a49c27beb1519fffa4db67e0e1d3d03
Joe Hancock <joe.hancock95@gmail.com>
2019-09-05 06:56:07 -0500

    got pictures working. syntax errors may be present

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Wrapper from './components/wrapper/Wrapper';
 import Card from './components/card/Card';
-import images from "./images.json";
+// import images from "./images.json";
 // import Footer from './components/footer/Footer';
 
-import './styles/main.css';
+// import './styles/main.css';
 
 class App extends Component {
-state = {
-  images
-};
+// state = {
+//   images
+// };
 
 // images is the json file
 // cardImage is what is being mapped
@@ -18,12 +18,13 @@ state = {
   render() {
     return (
       <Wrapper>
-        <Header></Header>
-        {this.state.images.map(cardImage => (
+        <Header/>
+        <Card/>
+        {/* {this.state.images.map(cardImage => (
           <Card
             id={cardImage.id}
             image={cardImage.image}
-          />
+          /> */}
         ))}
       </Wrapper>
     );
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import "./style.css";
+import pic01 from './images/pic01.png';
+import pic02 from './images/pic02.png';
+import pic03 from './images/pic03.png';
+import pic04 from './images/pic04.png';
+import pic05 from './images/pic05.png';
+import pic06 from './images/pic06.png';
+import pic07 from './images/pic07.png';
+import pic08 from './images/pic08.png';
+import pic09 from './images/pic09.png';
+import pic10 from './images/pic10.png';
+import pic11 from './images/pic11.png';
+import pic12 from './images/pic12.png';
 
 function Card(props) {
     return (
         <div className="card-container">
             <div className="img-container">
-                <img alt={props.name} src={props.image} />
+                <img src={pic01} alt="01"/>
+                <img src={pic02} alt="2"/>
+                <img src={pic03} alt="3"/>
+                <img src={pic04} alt="4"/>
+                <img src={pic05} alt="5"/>
+                <img src={pic06} alt="6"/>
+                <img src={pic07} alt="7"/>
+                <img src={pic08} alt="8"/>
+                <img src={pic09} alt="9"/>
+                <img src={pic10} alt="10"/>
+                <img src={pic11} alt="11"/>
+                <img src={pic12} alt="12"/>
+                
             </div>
         <span onClick={() => props.shuffleCard(props.id)}className="shuffle"></span>
         </div> 
